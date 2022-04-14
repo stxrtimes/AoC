ed30bb15f82b51fc526c56a07685c0d3c4fe0289
stxrtimes <adamski.davidson@btinternet.com>
2022-04-14 20:47:38 +0100

    Day 1 - Part 2

--- AoC/SonarSweep.cpp
+++ AoC/SonarSweep.cpp
@@ -5,9 +5,13 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
-int main()
+
+vector<int> ConvertFileToVectorInt();
+
+int Part1()
 {
 	ifstream input("input.txt");
 
@@ -23,5 +27,54 @@ int main()
 		}
 		prevScan = scan;
 	}
-	cout << counter << endl;
+	return counter;
+}
+
+int Part2()
+{
+	int scan = 0;
+	int prevScan = 0;
+
+	int counter = 0;
+
+	vector<int> input = ConvertFileToVectorInt();
+
+	for (int i = 0; i < input.size(); i++)
+	{
+		if (i < input.size() - 2)
+		{
+			scan = input[i] + input[i + 1] + input[i + 2];
+			if (i != 0 && scan > prevScan)
+			{
+				counter++;
+			}
+			prevScan = scan;
+		}
+	}
+
+	return counter;
+}
+
+int main()
+{
+
+	cout << "Answer for part 1: " << Part1() << endl;
+
+	cout << "Answer for part 2: " << Part2() << endl;
+}
+
+vector<int> ConvertFileToVectorInt()
+{
+	ifstream input("input.txt");
+
+	vector<int> fileVector;
+
+	int scan;
+	while (input >> scan)
+	{
+		fileVector.resize(fileVector.size() + 1);
+		fileVector[fileVector.size() - 1] = scan;
+	}
+
+	return fileVector;
 }
